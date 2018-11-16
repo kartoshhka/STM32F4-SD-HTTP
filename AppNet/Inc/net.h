3d896a37e7182f642149087e19431d0f46b305e9
Maria Bagulina <paralooid@gmail.com>
2018-11-16 16:43:39 +0300

    Initial commit. Starting with settings for tcp/ip, dhcp, http and FATFS mount

---
+++ AppNet/Inc/net.h
@@ -0,0 +1,19 @@
+#ifndef NET_H_
+#define NET_H_
+
+//-----------------------------------------------
+
+#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "lwip.h"
+#include "lwip/tcp.h"
+#include "netif.h"
+
+void tcp_server_init(void);
+void sendstring(char* buf_str);
+
+//-----------------------------------------------
+
+#endif /* NET_H_ */
---
+++ MDK-ARM/STM32F4 SD+HTTP/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+stm32f4 sd+http\startup_stm32f407xx.o: startup_stm32f407xx.s
